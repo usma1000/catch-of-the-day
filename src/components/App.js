3f0716147bf7203395db77aed470ce0708245241
usma1000 <usma1000@gmail.com>
2017-07-27 08:47:12 -0400

    added new fish form

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,23 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    const fishes = {...this.state.fishes};
+    const timeStamp = Date.now();
+    fishes[`fish-${timeStamp}`] = fish;
+    this.setState({fishes: fishes});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +28,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
