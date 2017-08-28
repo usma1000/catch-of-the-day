4027a664eab09354b9187dcb217783b838971205
usma1000 <usma1000@gmail.com>
2017-08-27 21:02:35 -0400

    created fishes component

---
+++ src/components/Fish.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Fish extends React.Component {
+  render() {
+    return (
+      <li className="menu-fish">
+        <img src={this.props.details.image} alt={this.props.details.name}/>
+
+      </li>
+    )
+  }
+}
+
+export default Fish;
